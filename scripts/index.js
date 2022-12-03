0ff711225defe96f927ed05ae051f38a7e4777f5
ops.error <anastasiia.inc@yandex.ru>
2022-12-03 20:41:34 +0300

    Исправлено: 1280: popup; 320: popup и страница

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ let editButton = page.querySelector('.profile__edit-button');
 let popup = page.querySelector('.popup');
 let profileName = page.querySelector('.profile__name');
 let profileDescription = page.querySelector('.profile__description');
-let nameInput = page.querySelector('.popup__input_name');
-let jobInput = page.querySelector('.popup__input_description');
+let nameInput = page.querySelector('.popup__input_type_name');
+let jobInput = page.querySelector('.popup__input_type_description');
 
 let closeButton = page.querySelector('.popup__close');
 
