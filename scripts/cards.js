f58b495a08b9570bbcc39c01602f77a9eb695d61
Анастасия Балакина <nasstya1107@gmail.com>
2023-02-03 03:05:46 +0300

    bugfix: Обновление картинок

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,32 @@
 const initialCards = [
     {
       name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+      link: 'https://i.pinimg.com/564x/a2/db/73/a2db73896e7d9785bc3ee3781c544fae.jpg'
     },
     {
       name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+      link: 'https://i.pinimg.com/736x/cb/10/80/cb1080920ed413750d51785e5a5946cd.jpg'
     },
     {
       name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+      link: 'https://i.pinimg.com/564x/f2/14/5b/f2145b15e0ec810a88a84cff132a3be1.jpg'
     },
     {
       name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+      link: 'https://i.pinimg.com/564x/26/dc/a2/26dca228571ba9593bea11cf332a8fbf.jpg'
     },
     {
       name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+      link: 'https://i.pinimg.com/564x/e0/b5/38/e0b538f226ce36bdab4a50deed65dae3.jpg'
     },
     {
       name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      // link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+      link: 'https://i.pinimg.com/564x/b8/e4/78/b8e478412aabec1defb1e067646cbb51.jpg'
     }
   ];
